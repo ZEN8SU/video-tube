e87515fffd39cd748dd9c47f87f81da440a19619
ZEN8SU <yashraajsrivastava@gmail.com>
2026-06-25 15:41:58 +0530

    connect or mongo atlas

---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config({path:'./env'}) consistency not maintained 
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+dotenv.config({
+  path: "./env",
+});
+
+
+
+connectDB();
+
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERR:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR :", error);
+    throw error;
+  }
+})();
+*/
